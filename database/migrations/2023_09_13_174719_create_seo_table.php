bf2330ae1751cc27f4dc6c62155c26c38bbbbb8d
htgdev <137365536+htgdev@users.noreply.github.com>
2023-09-13 23:25:02 +0800

    SEO , Author , BlogCategory

---
+++ database/migrations/2023_09_13_174719_create_seo_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSEOSTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('seo', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->longText('meta')->nullable();
+            $table->longText('header_bottom')->nullable();
+            $table->longText('body_top')->nullable();
+            $table->longText('body_bottom')->nullable();
+            $table->string('meta_title')->nullable();
+            $table->string('meta_description')->nullable();
+            $table->string('meta_keyword')->nullable();
+            $table->string('meta_canonical')->nullable();
+            $table->string('meta_ogimage')->nullable();
+            $table->string('site_name')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('s_e_o_s');
+    }
+}
