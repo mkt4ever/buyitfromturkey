d84b34179eba305d7f859781b669ac276133971c
Muhammed Khaled Tarakji <mktprogramming@gmail.com>
2023-10-18 01:54:01 +0300

    add toastr alerts

---
+++ public/js/custom.js
@@ -0,0 +1,20 @@
+toastr.options = {
+    closeButton: true,
+    debug: false,
+    newestOnTop: false,
+    progressBar: false,
+    positionClass: "toast-bottom-right",
+    preventDuplicates: false,
+    onclick: null,
+    extendedTImeout: 0,
+    setTimeout: 0,
+    // showDuration: "300",
+    // hideDuration: "1000",
+    // timeOut: "5000",
+    // extendedTimeOut: "1000",
+    showEasing: "swing",
+    // hideEasing: "linear",
+    showMethod: "fadeIn",
+    hideMethod: "fadeOut",
+  };
+  toastr.options.tapToDismiss = false;
