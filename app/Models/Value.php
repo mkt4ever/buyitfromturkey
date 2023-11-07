b04622be96c30f9d6647e09ac9f8a627f0798b45
MkharzomMAL <kharzom.mohammed.mal@gmail.com>
2023-11-07 21:30:47 +0800

    ourValues

---
+++ app/Models/Value.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
+use TCG\Voyager\Traits\Translatable;
+
+class Value extends Model implements Auditable
+{
+    use HasFactory, \OwenIt\Auditing\Auditable, Translatable;
+
+    protected $translatable = [
+        'title',
+        'slug',
+        'content',
+    ];
+}
